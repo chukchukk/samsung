aa20051fdcb4ac39a74718f6ed5344628bd228aa
chukchukk <kerityryui@mail.ru>
2019-11-08 00:15:25 +0300

    1.7.6.Примеры методов рисования фигур класса Canvas (Circle)

--- app/src/main/java/com/example/myapplication/MyView.java
+++ app/src/main/java/com/example/myapplication/MyView.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
@@ -14,11 +15,12 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        int y = 0;
-        while (y < canvas.getHeight()) {
-            canvas.drawLine(0, y,
-                    this.getWidth(), y, paint);
-            y += 30;
-        }
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(300, 300, 200, paint);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(300, 300, 200, paint);
     }
 }
