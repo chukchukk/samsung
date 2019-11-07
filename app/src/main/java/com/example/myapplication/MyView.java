91779e8d5e3579f170ea8954525cc19a48221993
chukchukk <kerityryui@mail.ru>
2019-11-08 00:58:57 +0300

    1.7.7. Uzor

--- app/src/main/java/com/example/myapplication/MyView.java
+++ app/src/main/java/com/example/myapplication/MyView.java
@@ -16,7 +16,32 @@ public class MyView extends View {
         super.onDraw(canvas);
         Paint paint = new Paint();
         paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(100, 100, 300, 300, paint);
+        paint.setStrokeWidth(5);
+        int i=0;
+        while(i<canvas.getHeight())
+        {
+            canvas.drawLine(i,0,canvas.getWidth(),canvas.getHeight()-i,paint);
+            if(i>canvas.getHeight()/3)
+                paint.setColor(Color.GRAY);
+            i+=50;
+        }
+        paint.setColor(Color.BLUE);
+        i=0;
+        while(i<canvas.getHeight())
+        {
+            canvas.drawLine(0,i,canvas.getWidth()-i,canvas.getHeight(),paint);
+            if(i>canvas.getHeight()/4)
+                paint.setColor(Color.RED);
+            i+=50;
+        }
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(canvas.getWidth()/2,canvas.getHeight()/2,250,paint);
+        paint.setColor(Color.WHITE);
+        canvas.drawCircle(canvas.getWidth()/2,canvas.getHeight()/2,240,paint);
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(50);
+        canvas.drawText("А что, патриотично",canvas.getWidth()/2-220,canvas.getHeight()/2,paint);
+
+
     }
 }
