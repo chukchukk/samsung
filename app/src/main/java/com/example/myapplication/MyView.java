53c3a3c8c2bc77dafd36a1e515f098f424cad5cd
chukchukk <kerityryui@mail.ru>
2019-11-08 01:15:07 +0300

    1.8.7. Простейшая анимация

--- app/src/main/java/com/example/myapplication/MyView.java
+++ app/src/main/java/com/example/myapplication/MyView.java
@@ -8,40 +8,24 @@ import android.view.View;
 
 public class MyView extends View {
     public MyView(Context context) {
+
         super(context);
+
     }
+    long lastTime = System.currentTimeMillis();
+    float x=0;
 
     @Override
+
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(5);
-        int i=0;
-        while(i<canvas.getHeight())
-        {
-            canvas.drawLine(i,0,canvas.getWidth(),canvas.getHeight()-i,paint);
-            if(i>canvas.getHeight()/3)
-                paint.setColor(Color.GRAY);
-            i+=50;
-        }
-        paint.setColor(Color.BLUE);
-        i=0;
-        while(i<canvas.getHeight())
-        {
-            canvas.drawLine(0,i,canvas.getWidth()-i,canvas.getHeight(),paint);
-            if(i>canvas.getHeight()/4)
-                paint.setColor(Color.RED);
-            i+=50;
-        }
-        paint.setColor(Color.BLACK);
-        canvas.drawCircle(canvas.getWidth()/2,canvas.getHeight()/2,250,paint);
-        paint.setColor(Color.WHITE);
-        canvas.drawCircle(canvas.getWidth()/2,canvas.getHeight()/2,240,paint);
-        paint.setColor(Color.BLACK);
-        paint.setTextSize(50);
-        canvas.drawText("А что, патриотично",canvas.getWidth()/2-220,canvas.getHeight()/2,paint);
 
+        canvas.drawCircle(x, 300, 20, paint);
+        long nowTime = System.currentTimeMillis();
+        x += 0.01f * (nowTime - lastTime);
+        lastTime = nowTime;
+        invalidate();
 
     }
 }
