6e70535214f134dba57fa50a9bd7da8da0ac4d45
chukchukk <kerityryui@mail.ru>
2019-11-08 12:13:23 +0300

    1.11.6.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,135 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+
+
+public class MainActivity extends Activity implements OnClickListener,
+        OnLongClickListener {
+
+    private int WIDTH = 10;
+    private int HEIGHT = 10;
+
+    private Button[][] cells;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
+        setContentView(R.layout.cells);
+        makeCells();
+
+        generate();
+
     }
+
+    void generate() {
+
+        //Эту строку нужно удалить
+        int num = 1;
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++) {
+                cells[i][j].setText(num + "");
+                num++;
+            }
+
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++) {
+                //ADD YOUR CODE HERE
+                //....
+                cells[i][j].setBackgroundColor(Color.YELLOW);
+            }
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        //Эту строку нужно удалить
+
+        /*Button tappedCell = (Button) v;
+        //Получаем координтаты нажатой клетки
+        int tappedX = getX(tappedCell);
+        int tappedY = getY(tappedCell);
+        //ADD YOUR CODE HERE
+        //....
+        for (int x = 0; x < WIDTH; x++)
+        {
+            cells[tappedY][x].setBackgroundColor(Color.RED);
+        }
+        for (int y = 0; y < WIDTH; y++)
+        {
+            cells[y][tappedX].setBackgroundColor(Color.RED);
+        }*/
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        //Эту строку нужно удалить
+
+
+        Button tappedCell = (Button) v;
+
+        //Получаем координтаты нажатой клетки
+        int tappedX = getX(tappedCell);
+        int tappedY = getY(tappedCell);
+        //ADD YOUR CODE HERE
+        //....
+        int color = ((ColorDrawable)cells[tappedY][tappedX].getBackground()).getColor();
+       /* for (int x = 0; x < WIDTH; x++) {
+            if (color == Color.YELLOW) {
+                cells[tappedY][x].setBackgroundColor(Color.RED);}
+            else {
+                cells[tappedY][x].setBackgroundColor(Color.YELLOW);
+            }
+        }*/
+        for (int y = 0; y < WIDTH; y++) {
+            if (color == Color.RED) {
+                cells[y][tappedX].setBackgroundColor(Color.YELLOW);}
+            else {
+                cells[y][tappedX].setBackgroundColor(Color.RED);
+            }
+        }
+    }
+
+    /*
+     * NOT FOR THE BEGINNERS
+     * ==================================================
+     */
+
+    int getX(View v) {
+        return Integer.parseInt(((String) v.getTag()).split(",")[1]);
+    }
+
+    int getY(View v) {
+        return Integer.parseInt(((String) v.getTag()).split(",")[0]);
+    }
+
+    void makeCells() {
+        cells = new Button[HEIGHT][WIDTH];
+        GridLayout cellsLayout = (GridLayout) findViewById(R.id.CellsLayout);
+        cellsLayout.removeAllViews();
+        cellsLayout.setColumnCount(HEIGHT);
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++) {
+                LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                cells[i][j] = (Button) inflater.inflate(R.layout.cell, cellsLayout, false);
+                cells[i][j].setOnClickListener(this);
+                cells[i][j].setOnLongClickListener(this);
+                cells[i][j].setTag(i + "," + j);
+                cellsLayout.addView(cells[i][j]);
+            }
+    }
+
 }
+
--- app/src/main/java/com/example/myapplication/MyView.java
+++ app/src/main/java/com/example/myapplication/MyView.java
@@ -1,90 +1,111 @@
 package com.example.myapplication;
 
 
+import android.app.Activity;
 import android.content.Context;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 
-public class MyView extends View {
-    Paint paint = new Paint();
-    int N = 10; // количество шариков
-    float[] x  = new float[N];
-    float[] y  = new float[N];
-    float[] vx = new float[N];
-    float[] vy = new float[N];
-    float[] rad=new float[N];
-    float [] col=new float[N];
 
-    public MyView(Context context) {
-        super(context);
-        fillRandom(x, 0, 500);
-        fillRandom(y, 0, 500);
-        fillRandom(vx, -3, 3);
-        fillRandom(vy, -2,0);
-        fillRandom(rad,40,60);
-        fillRandom(col,150,165);
+
+public class MyView extends Activity implements OnClickListener, OnLongClickListener {
+
+    private int WIDTH = 15;
+    private int HEIGHT = 10;
+
+    private Button[][] cells;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cell);
+        makeCells();
+
+        generate();
+
+    }
+
+    void generate() {
+
+
+
+        int num = 1;
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++) {
+                cells[i][j].setText(num + "");
+                num++;
+            }
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++)
+                if (Math.random() >= 0.5)
+                {
+                    cells[i][j].setBackgroundColor(Color.YELLOW);
+                }
     }
 
     @Override
+    public boolean onLongClick(View v) {
 
-    protected void onDraw(Canvas canvas) {
+        return false;
+    }
 
-        drawBalls(canvas);
-        for(int i=0;i<N;i++)
+    @Override
+    public void onClick(View v) {
+
+
+        Button tappedCell = (Button) v;
+
+        //Получаем координтаты нажатой клетки
+        int tappedX = getX(tappedCell);
+        int tappedY = getY(tappedCell);
+        for (int x = 0; x < WIDTH; x++)
         {
-            moveballs(x,i);
-            add(x, vx);
-            moveballsy(y,i);
-            add(y, vy);
+            cells[tappedY][x].setBackgroundColor(Color.RED);
         }
-        invalidate();
-
-    }
-    void moveballs(float []x,int i ) {
-        if (x[i] < 0 + rad[i] || x[i] > this.getWidth() - rad[i]) {
-            vx[i] = -vx[i];
-            //  rad[i]+=10;
-
-        }
-    }
-    void moveballsy(float []y,int i )
-    {
-        if (y[i] < 0+rad[i] || y[i] > this.getHeight()-rad[i])
+        for (int y = 0; y < WIDTH; y++)
         {
-            vy[i] = - vy[i];
-            //rad[i]+=5;
-
+            cells[y][tappedX].setBackgroundColor(Color.RED);
         }
 
     }
-    float rand(float min , float max){
-        return (float)(Math.random() * (max - min + 1)) + min;
-    }
-    void fillRandom(float[] array , float min, float max){
-        for (int i = 0; i < array.length; i++){
-            array[i] = rand (min, max);
-        }
-    }
-    void add(float[] array , float[] values){
-        for (int i = 0; i < array.length; i++){
-            array[i] += values[i];
-        }
-    }
-    void drawBalls(Canvas canvas)
-    {
-        for (int i = 0; i < N - 1; i++) {
 
-            canvas.drawLine(x[i], y[i], x[i + 1], y[i + 1], paint);
 
-        }
-        for (int i = 0; i < N; i++) {
-            paint.setColor(Color.rgb((int)col[i]+23,(int)col[i]+50,(int)col[i]-15));
-            canvas.drawCircle(x[i], y[i], rad[i], paint);
-        }
+
+
+
+    int getX(View v) {
+        return Integer.parseInt(((String) v.getTag()).split(",")[1]);
     }
+
+    int getY(View v) {
+        return Integer.parseInt(((String) v.getTag()).split(",")[0]);
+    }
+
+    void makeCells() {
+        cells = new Button[HEIGHT][WIDTH];
+        GridLayout cellsLayout = (GridLayout) findViewById(R.id.CellsLayout);
+        cellsLayout.removeAllViews();
+        cellsLayout.setColumnCount(HEIGHT);
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++) {
+                LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                cells[i][j] = (Button) inflater.inflate(R.layout.cell, cellsLayout, false);
+                cells[i][j].setOnClickListener(this);
+                cells[i][j].setOnLongClickListener(this);
+                cells[i][j].setTag(i + "," + j);
+                cellsLayout.addView(cells[i][j]);
+            }
+    }
+
 }
 
 
